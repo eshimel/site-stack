6edac90c95c2b15f7496cdaf4a7eb6b44ebeccfa
Ian Cullinane <icullinane@turbine.com>
2015-08-23 23:42:16 -0400

    remote database connected

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://45.55.50.51:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     //mongodb://username:password@host:port
 };
