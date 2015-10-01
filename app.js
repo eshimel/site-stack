c46ea959bbb4361faf580c440e64020e911e708e
Ian Cullinane <ian.cullinane@gmail.com>
2015-10-01 09:05:45 -0400

    Card layout for tracks

--- app.js
+++ app.js
@@ -7,17 +7,19 @@
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
 
+ /*jslint node: true */
+'use strict';
+
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var passport = require('passport');
-var session = require('express-session')
+var session = require('express-session');
 var path = require('path');
 var flash = require('connect-flash');
 
-// define the logger
-var logger = require('morgan');
+
 
 // pass passport for configuration
 //require('./config/passport-config')(passport); 
@@ -27,10 +29,13 @@ var app = express();
 
 // required for passport
 
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true }))
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash()); // use connect-flash for flash messages stored in session
+
+// use the static page stuffs
+app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use(express.static(__dirname + '/public'));
 
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@
 
 var express = require('express');
 var request = require('request');
+
+// define the logger
+var logger = require('morgan');
+
+// the Router subclass to be exported
 var router = express.Router();
 
 //var Article = require('../models/article');
@@ -15,7 +20,12 @@ var router = express.Router();
 //});
 //
 
-var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&limit=1&user=ianpants&api_key=57ee3318536b23ee81d6b27e36997cde&format=json';
+var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&limit=10&user=ianpants&api_key=57ee3318536b23ee81d6b27e36997cde&format=json';
+
+
+// logging middleware for debug
+router.use(logger('dev'));
+
 
 //var options = {
 //  host: 'http://ws.audioscrobbler.com/2.0/',
@@ -28,7 +38,6 @@ var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&limit=1
 function getData(callback) {
     request(url, function (err, res, body) {
         if (!err && res.statusCode == 200) {
-          console.log('getData called on body: ' + typeof body); // Show the HTML for the Google homepage.
           callback(null, res.body);
         } else {
           callback(res.statusCode);
@@ -43,22 +52,11 @@ router.get('/', function(req, res, next) {
       if (err) {
         return next(err);
       }
-      console.log('data coming back to router: ' + typeof data);
       var jdata = JSON.parse(data);
-      console.log('after conversion: ' + typeof jdata);
-      console.log(jdata.recenttracks.track[0].name);
+      console.log(jdata.recenttracks.track[0].artist);
       res.render('index', {title: 'Home', data: jdata });
     });
 });
-
-
-
-/* GET home page. */
-router.get('/sample', function(req, res, next) {
-  res.send('pancakes!');
-});
-
-
 
 
 module.exports = router;
